c159dea8da40a077fb3e2d71925b7e65abb92a4b
Mark Tiele Westra <m.t.westra@akvo.org>
2016-01-22 13:52:07 +0100

    [#1] Lexical scoping Coursera programming example

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Defines functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
-
+## Creates a "matrix" object that can cache its inverse.
+## It uses the joint enclosing environment of the created functions to store the state,
+## which consists of the matrix and the inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(i) inv <<- i
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Computes the inverse of the "matrix" object defined using the makeCacheMatrix.
+## If the inverse already has been computed before, it is returned immediately.
+## If the inverse has not been computed before, it is computed and cached.
+## The function assumes that the matrix x is invertible; no error handling is performed.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        message("getting cached matrix inverse")
+        return(inv)
+    }
+    mat <- x$get()
+    inv <- solve(mat, ...)
+    x$setInverse(inv)
+    inv
 }
